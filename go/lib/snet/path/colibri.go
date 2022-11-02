cdeb0a6ceffec3cecc74c6df57417f07e746c6bd
Juan A. Garcia Pardo <juagargi@gmail.com>
2022-11-02 11:48:51 +0100

    EER timestamps are calculated at the colibri gateway

--- go/lib/snet/path/colibri.go
+++ go/lib/snet/path/colibri.go
@@ -15,37 +15,22 @@
 package path
 
 import (
-	"time"
-
-	libcolibri "github.com/scionproto/scion/go/lib/colibri/dataplane"
 	"github.com/scionproto/scion/go/lib/slayers"
-	"github.com/scionproto/scion/go/lib/slayers/path/colibri"
+	colpath "github.com/scionproto/scion/go/lib/slayers/path/colibri"
 )
 
 // Colibri represents a Colibri path in the data plane. For now, only static MACs are supported.
 type Colibri struct {
 	// Raw is the raw representation of this path.
 	Raw []byte
-
-	counter uint32
 }
 
 func (p Colibri) SetPath(s *slayers.SCION) error {
-	var colPath colibri.ColibriPathMinimal
+	var colPath colpath.ColibriPathMinimal
 	if err := colPath.DecodeFromBytes(p.Raw); err != nil {
 		return err
 	}
 	colPath.InfoField.OrigPayLen = s.PayloadLen
-
-	// For EER data packets add a high-precision timestamp
-	if !colPath.InfoField.C {
-		tsRel, err := libcolibri.CreateTsRel(colPath.InfoField.ExpTick, time.Now())
-		if err != nil {
-			return err
-		}
-		colPath.PacketTimestamp = libcolibri.CreateColibriTimestamp(tsRel, 0, p.counter)
-		p.counter += 1
-	}
 
 	s.Path, s.PathType = &colPath, colPath.Type()
 	return nil
